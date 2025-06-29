ab0bc1903adea8b2797ab09a9d7baddba85deacb
Mekdes D <mekdideriba3@gmail.com>
2025-06-29 15:51:41 +0300

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -38,5 +38,5 @@ CREATE TABLE IF NOT EXISTS ORDER_DETAILS (
     book_id INT,
     quantity INT,
     FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
-    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id),"quantity DOUBLE", "FOREIGN KEY (order_id) REFERENCES Orders(order_id)", "FOREIGN KEY (book_id) REFERENCES Books(book_id)"
 );
